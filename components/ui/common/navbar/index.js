b427280d02c54193870ec0e62103c63ad990815b
VARUGU DIVAKAR REDDY <divakarreddyvarugu@gmail.com>
2022-09-02 17:07:46 +0530

    account display
    small update

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,71 +1,72 @@
 import { useWeb3 } from "@components/provider";
-import Link from "next/link"
-import { Button } from "@components/ui/common"
-import { useAccount } from "@components/web3/hooks/useAccount"
+import Link from "next/link";
+import { Button } from "@components/ui/common";
+import { useAccount } from "@components/web3/hooks/useAccount";
 
 export default function Footer() {
-  const { connect, isLoading, isWeb3Loaded } = useWeb3()
-  const { account } = useAccount()
+  const { connect, isLoading, isWeb3Loaded } = useWeb3();
+  const { account } = useAccount();
 
   return (
     <section>
-      {account}
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
           <div className="flex justify-between items-center">
             <div>
-              <Link href="/" >
-                <a
-                  className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Home
                 </a>
               </Link>
-              <Link href="/" >
-                <a
-                  className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Marketplace
                 </a>
               </Link>
-              <Link href="/" >
-                <a
-                  className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Blogs
                 </a>
               </Link>
             </div>
             <div>
-              <Link href="/" >
-                <a
-                  className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+              <Link href="/">
+                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Wishlist
                 </a>
               </Link>
-              { isLoading ?
+              {isLoading ? (
+                <Button disabled={true} onClick={connect}>
+                  Loading...
+                </Button>
+              ) : isWeb3Loaded ? (
+                account ? (
+                  <Button hoverable={false} className="cursor-default">
+                    Hi there
+                  </Button>
+                ) : (
+                  <Button onClick={connect}>Connect</Button>
+                )
+              ) : (
                 <Button
-                  disabled={true}
-                  onClick={connect}>
-                    Loading...
-                </Button> :
-                isWeb3Loaded ?
-                account ?
-                <Button
-                  hoverable={false}
-                  className="cursor-default">
-                  Hi there
-                </Button> :
-                <Button
-                  onClick={connect}>
-                  Connect
-                </Button> :
-                <Button
-                  onClick={() => window.open("https://metamask.io/download.html", "_blank")}>
+                  onClick={() =>
+                    window.open("https://metamask.io/download.html", "_blank")
+                  }
+                >
                   Install Metamask
                 </Button>
-              }
+              )}
             </div>
           </div>
         </nav>
       </div>
+      {account && (
+        <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
+          <div className="text-white bg-indigo-600 rounded-md p-2">
+            {account}
+          </div>
+        </div>
+      )}
     </section>
-  )
+  );
 }
