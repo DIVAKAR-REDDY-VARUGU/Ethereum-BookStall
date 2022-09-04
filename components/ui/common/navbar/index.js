e321ebb273fa8a9eb89e17087847cf0b8c1dd685
VARUGU DIVAKAR REDDY <divakarreddyvarugu@gmail.com>
2022-09-04 10:29:32 +0530

    hiding address on marketplace

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -2,10 +2,12 @@ import { useWeb3 } from "@components/provider";
 import Link from "next/link"
 import { Button } from "@components/ui/common"
 import { useAccount } from "@components/hooks/web3/useAccount"
+import { useRouter } from "next/router"
 
 export default function Footer() {
   const { connect, isLoading, isWeb3Loaded } = useWeb3()
   const { account } = useAccount()
+  const { pathname } = useRouter()
 
   return (
     <section>
@@ -66,6 +68,7 @@ export default function Footer() {
         </nav>
       </div>
       { account.data &&
+        !pathname.includes("/marketplace") &&
         <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
           <div className="text-white bg-indigo-600 rounded-md p-2">
             {account.data}
