0250d38a6d848d4d0f092c8d90edae133485bfe5
VARUGU DIVAKAR REDDY <divakarreddyvarugu@gmail.com>
2022-09-04 10:06:50 +0530

    changes for Admin

--- components/provider/web3/hooks/useAccount.js
+++ components/provider/web3/hooks/useAccount.js
@@ -1,35 +1,30 @@
-
-
-import { useEffect } from "react"
-import useSWR from "swr"
+import { useEffect } from "react";
+import useSWR from "swr";
 
 const adminAddresses = {
-  "0xdb7B40398ee7594E151E1ABc69537991ecFF0762": true,
+  "0x75dd86ed96f7a87b6ffa7db6c5948004eab2de06cb5803516f95513992cd15ee": true,
 };
 
 export const handler = (web3, provider) => () => {
-
-  const { data, mutate, ...rest } = useSWR(() =>
-    web3 ? "web3/accounts" : null,
+  const { data, mutate, ...rest } = useSWR(
+    () => (web3 ? "web3/accounts" : null),
     async () => {
-      const accounts = await web3.eth.getAccounts()
-      return accounts[0]
+      const accounts = await web3.eth.getAccounts();
+      return accounts[0];
     }
-  )
+  );
 
   useEffect(() => {
     provider &&
-    provider.on("accountsChanged",
-      accounts => mutate(accounts[0] ?? null)
-    )
-  }, [provider])
+      provider.on("accountsChanged", (accounts) => mutate(accounts[0] ?? null));
+  }, [provider]);
 
   return {
     account: {
       data,
-      isAdmin: (data && adminAddresses[data]) ?? false,
+      isAdmin: (data && adminAddresses[web3.utils.keccak256(data)]) ?? false,
       mutate,
-      ...rest
-    }
-  }
-}
+      ...rest,
+    },
+  };
+};
