3e9f238b2763f8ae5b4eb778f937c2908d48f980
VARUGU DIVAKAR REDDY <divakarreddyvarugu@gmail.com>
2022-09-13 15:18:21 +0530

    card wrappers

--- components/ui/course/card/index.js
+++ components/ui/course/card/index.js
@@ -1,9 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
 
-export default function Card({ course }) {
+
+import Image from "next/image"
+import Link from "next/link"
+
+export default function Card({course, Footer}) {
   return (
-    <div className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div
+      className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       <div className="flex h-full">
         <div className="flex h-full">
           <Image
@@ -16,17 +19,25 @@ export default function Card({ course }) {
           />
         </div>
         <div className="p-8">
-          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+          <div
+            className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
             {course.type}
           </div>
           <Link href={`/courses/${course.slug}`}>
-            <a className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+            <a
+              className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
               {course.title}
             </a>
           </Link>
-          <p className="mt-2 text-gray-500">{course.description}</p>
+          <p
+            className="mt-2 text-gray-500">
+            {course.description}
+          </p>
+          { Footer &&
+            <Footer />
+          }
         </div>
       </div>
     </div>
-  );
+  )
 }
--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -1,12 +1,13 @@
-import { CourseCard, CourseList } from "@components/ui/course"
-import { BaseLayout } from "@components/ui/layout"
-import { getAllCourses } from "@content/courses/fetcher"
-import { WalletBar } from "@components/ui/web3"
-import { useAccount, useNetwork } from "@components/hooks/web3"
+import { CourseCard, CourseList } from "@components/ui/course";
+import { BaseLayout } from "@components/ui/layout";
+import { getAllCourses } from "@content/courses/fetcher";
+import { WalletBar } from "@components/ui/web3";
+import { useAccount, useNetwork } from "@components/hooks/web3";
+import { Button } from "@components/ui/common";
 
-export default function Marketplace({courses}) {
-  const { account } = useAccount()
-  const { network } = useNetwork()
+export default function Marketplace({ courses }) {
+  const { account } = useAccount();
+  const { network } = useNetwork();
 
   return (
     <>
@@ -17,31 +18,34 @@ export default function Marketplace({courses}) {
             data: network.data,
             target: network.target,
             isSupported: network.isSupported,
-            hasInitialResponse: network.hasInitialResponse
+            hasInitialResponse: network.hasInitialResponse,
           }}
         />
       </div>
-      <CourseList
-        courses={courses}
-      >
-      {course =>
-        <CourseCard
-          key={course.id}
-          course={course}
-        />
-      }
+      <CourseList courses={courses}>
+        {(course) => (
+          <CourseCard
+            key={course.id}
+            course={course}
+            Footer={() => (
+              <div className="mt-4">
+                <Button variant="lightPurple">Purchase</Button>
+              </div>
+            )}
+          />
+        )}
       </CourseList>
     </>
-  )
+  );
 }
 
 export function getStaticProps() {
-  const { data } = getAllCourses()
+  const { data } = getAllCourses();
   return {
     props: {
-      courses: data
-    }
-  }
+      courses: data,
+    },
+  };
 }
 
-Marketplace.Layout = BaseLayout
+Marketplace.Layout = BaseLayout;
