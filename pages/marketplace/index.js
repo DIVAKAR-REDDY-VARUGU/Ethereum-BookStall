8785637ade40691f574e9233543e3eddd3023081
VARUGU DIVAKAR REDDY <divakarreddyvarugu@gmail.com>
2022-09-13 15:24:40 +0530

    Model

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -1,13 +1,14 @@
-import { CourseCard, CourseList } from "@components/ui/course";
-import { BaseLayout } from "@components/ui/layout";
-import { getAllCourses } from "@content/courses/fetcher";
-import { WalletBar } from "@components/ui/web3";
-import { useAccount, useNetwork } from "@components/hooks/web3";
-import { Button } from "@components/ui/common";
 
-export default function Marketplace({ courses }) {
-  const { account } = useAccount();
-  const { network } = useNetwork();
+import { CourseCard, CourseList } from "@components/ui/course"
+import { BaseLayout } from "@components/ui/layout"
+import { getAllCourses } from "@content/courses/fetcher"
+import { WalletBar } from "@components/ui/web3"
+import { useAccount, useNetwork } from "@components/hooks/web3"
+import { Button, Modal } from "@components/ui/common"
+
+export default function Marketplace({courses}) {
+  const { account } = useAccount()
+  const { network } = useNetwork()
 
   return (
     <>
@@ -18,34 +19,39 @@ export default function Marketplace({ courses }) {
             data: network.data,
             target: network.target,
             isSupported: network.isSupported,
-            hasInitialResponse: network.hasInitialResponse,
+            hasInitialResponse: network.hasInitialResponse
           }}
         />
       </div>
-      <CourseList courses={courses}>
-        {(course) => (
-          <CourseCard
-            key={course.id}
-            course={course}
-            Footer={() => (
-              <div className="mt-4">
-                <Button variant="lightPurple">Purchase</Button>
-              </div>
-            )}
-          />
-        )}
+      <CourseList
+        courses={courses}
+      >
+      {course =>
+        <CourseCard
+          key={course.id}
+          course={course}
+          Footer={() =>
+            <div className="mt-4">
+              <Button variant="lightPurple">
+                Purchase
+              </Button>
+            </div>
+          }
+        />
+      }
       </CourseList>
+      <Modal isOpen={false} />
     </>
-  );
+  )
 }
 
 export function getStaticProps() {
-  const { data } = getAllCourses();
+  const { data } = getAllCourses()
   return {
     props: {
-      courses: data,
-    },
-  };
+      courses: data
+    }
+  }
 }
 
-Marketplace.Layout = BaseLayout;
+Marketplace.Layout = BaseLayout
