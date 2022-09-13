1d0e1b2a59f8bc07eeb2b3b58762967a19840d8b
VARUGU DIVAKAR REDDY <divakarreddyvarugu@gmail.com>
2022-09-13 15:52:32 +0530

    orders component updates

--- components/ui/common/index.js
+++ components/ui/common/index.js
@@ -13,5 +13,5 @@ export { default as Navbar } from "./navbar";
 export { default as Footer } from "./footer";
 export { default as Hero } from "./hero";
 export { default as Breadcrumbs } from "./breadcrumbs";
-export { default as Model } from "./model";
+export { default as Model } from "./modal";
 export { default as Button } from "./button";
--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -1,12 +1,16 @@
 
+
 import { CourseCard, CourseList } from "@components/ui/course"
 import { BaseLayout } from "@components/ui/layout"
 import { getAllCourses } from "@content/courses/fetcher"
 import { WalletBar } from "@components/ui/web3"
 import { useAccount, useNetwork } from "@components/hooks/web3"
-import { Button, Modal } from "@components/ui/common"
+import { Button } from "@components/ui/common"
+import { OrderModal } from "@components/ui/order"
+import { useState } from "react"
 
 export default function Marketplace({courses}) {
+  const [selectedCourse, setSelectedCourse] = useState(null)
   const { account } = useAccount()
   const { network } = useNetwork()
 
@@ -32,7 +36,9 @@ export default function Marketplace({courses}) {
           course={course}
           Footer={() =>
             <div className="mt-4">
-              <Button variant="lightPurple">
+              <Button
+                onClick={() => setSelectedCourse(course)}
+                variant="lightPurple">
                 Purchase
               </Button>
             </div>
@@ -40,7 +46,12 @@ export default function Marketplace({courses}) {
         />
       }
       </CourseList>
-      <Modal isOpen={false} />
+      { selectedCourse &&
+        <OrderModal
+          course={selectedCourse}
+          onClose={() => setSelectedCourse(null)}
+        />
+      }
     </>
   )
 }
